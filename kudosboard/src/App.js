27283250b884cf7338ed06cdbec147b9ca70284f
Blessing Amoah <blessing@meta.com>
2024-06-18 16:54:57 -0700

    sdklfl

--- kudosboard/src/App.js
+++ kudosboard/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   const fetchBoards = async () => {
     try {
-      const response = await fetch(process.env.REACT_APP_API_URL + '/api/boards');
+      const response = await fetch('https://backend-ldgy.onrender.com/api/boards');
       if (response.ok) {
         throw new Error(`Something went wrong ${response.status}`);
     }
