d5f9d687e1bb0b83524cd8e0ed651083d09134a5
Blessing Amoah <blessing@meta.com>
2024-06-18 17:29:19 -0700

    fd

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,11 @@ const port = process.env.PORT || 3002
 
 
 
+const config = require('./config');
+
+// Use the config variables
+console.log(config.DATABASE_HOSTED_URL);
+
 app.use(cors())
 app.use(express.json())
 
@@ -27,7 +32,6 @@ const cardRoutes = require('./routes/cardRoutes')
 
 app.use('/api/boards', boardRoutes);
 app.use('/api/cards', cardRoutes);
-
 
 
 app.listen(port, () => {
